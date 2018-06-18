aef09cef133acb1c6d007da8209002ce94e433b5
ava azama <avaazama@gmail.com>
2018-06-18 20:08:24 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -6,7 +6,7 @@ puts "The correct sequence of steps for the Macarena are:"
 # The first instruction is correct. 
 
 # Step 1: GOOD
-puts "Step 1: "Right arm out in front of you, palm facing down."
+puts ""Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
 puts "Step 2: "Left arm out in front of you, palm facing down."
