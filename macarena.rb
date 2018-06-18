c0ef6eafd059c3c87dd10ee231109a102bb842a5
ava azama <avaazama@gmail.com>
2018-06-18 20:16:13 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,7 +1,6 @@
 def Macarena
 puts "How To Do The Macarena:"
-# puts "How To Do The Macarena:"
-
+puts""
 # Fix The Dance Instructions Below
 # Every line should have one instruction using puts.
 # The first instruction is correct. 
