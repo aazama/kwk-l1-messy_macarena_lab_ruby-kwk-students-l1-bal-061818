cbcbc81922bb21bcb751301946d80794bc46e88f
ava azama <avaazama@gmail.com>
2018-06-18 20:11:15 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,3 +1,4 @@
+defined Macarena
 puts "How To Do The Macarena:"
 puts "The correct sequence of steps for the Macarena are:"
 
@@ -9,7 +10,7 @@ puts "The correct sequence of steps for the Macarena are:"
 puts "Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
-puts  "Left arm out in front of you, palm facing down."
+puts "Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
 puts "Turn your right palm up."
@@ -27,5 +28,6 @@ puts  "Place your left hand on your left shoulder."
 puts  "Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
-puts  "Bring your left hand up and around back your head."
+puts "Bring your left hand up and around back your head."
 end
+Macarena
